96de815e67ed5747e3f6504cda0a1ef5477fc5ee
sebastianmontdev <sebastianmont-1199@hotmail.com>
2025-11-19 00:00:15 -0500

    Add files via upload

--- inventario2.py
+++ inventario2.py
@@ -1,4 +1,7 @@
-lista_usuarios = []
+inventario = [
+     {"nombre" : "papa","precio" : 2.5, "cantidad" : 5},
+     {"nombre" : "banano","precio" : 1.0, "cantidad" : 3}
+     ]
 
 def agregar(nombre, precio, cantidad):
         usuario = {}
@@ -9,8 +12,6 @@ def agregar(nombre, precio, cantidad):
         return usuario
 
 inicio = True
-producto = {"nombre" : "papa","precio" : 500 ,"cantidad" : 50}
-inventario = [producto]
 while inicio == True:
     print("1.Agregar producto")
     print("2. Mostrar inventario")
@@ -19,49 +20,22 @@ while inicio == True:
     opcion = int(input("ingrese una opcion"))
     if opcion == 1:
         while True:
-            #declaramos la variable nombre
-            nombre = input("ingrese el nombre del producto")
-            #ponemos un condicional para asegurarnos de que el dato sea texto
-            #isalpha revisa si los datos son texto sin espacios ni careacteres especiales
-            if nombre.isalpha():
-                #todo funciono
-                print("dato guardado")
-            #else por si no se cumple la condicion
+            nombre = input("ingrese nombre")
+            precio = float(input("ingrese precio"))
+            cantidad = int(input("ingrese la cantida"))
+
+            nuevo_usuario = agregar(nombre, precio, cantidad)
+            inventario.append(nuevo_usuario)
+            print(inventario)
+            salir_registro = input("desea registrar otro producto?")
+            if salir_registro == "si":
+                 print("inicia proceso de registro")
+                 continue
             else:
-                #mensajes de error si no se cumple la condicion
-                print("porfavor ingrese solo texto sin espacios")
-            #abrimos un bucle para errores
-            while True:
-                #lo usamos para atrapar errores
-                try:
-                    #declaramos la variable precio
-                    precio = float(input("ingrese el precio"))
-                    #todo funciono
-                    print("dato guardado")
-                    #para salir del bucle
-                    break 
-                #si atrapa un erro mostra el mensaje
-                except ValueError:
-                    print("ingrese un valor correcto")
-                    #abrimos un bucle para errores
-            while True:
-                #lo usamos para atrapar errores
-                try:
-                    #declaramos la variable cantidad
-                    cantidad = int(input("ingrese la cantidad de ese producto"))
-                    #todo funciono
-                    print("dato guardado")
-                    #para salir del bucle
-                    break 
-                #si atrapa un erro mostra el mensaje
-                except ValueError:
-                    print("ingrese numeros enteros")
-            #aqui guardamos el precio final
-            costo_total = cantidad * precio
-            #aqui mostramos la "factura"
-            print(f"producto:{nombre} precio:{precio} total:{costo_total}")
+                break
     elif opcion == 2:
-        print(inventario)
+        for item in inventario:
+            print(f"Producto: {item['nombre']} | Precio: {item['precio']} | Cantidad: {item['cantidad']}")
     elif opcion == 3:
         print()
     elif opcion == 4:
